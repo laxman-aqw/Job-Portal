3e5a67f09df2e554df9ea92d73af849aabd9062e
Laxman Rumba <rumbavai767@gmail.com>
2025-01-30 22:13:21 +0545

    fixed webhooks controller

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -1,26 +1,24 @@
 const { Webhook } = require("svix");
-
 const mongoose = require("mongoose");
-
 const User = require("../models/User");
 
-//api controller function to manage clerk user with database
+// API controller function to manage Clerk user with database
 const clerkWebHook = async (req, res) => {
   try {
-    //create a svix indtance with clerk webhook secret
+    // Create a svix instance with Clerk webhook secret
     const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    //verifiying headers
+    // Verifying headers
     await webhook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
 
-    //getting data from the userbody
+    // Getting data from the request body
     const { data, type } = req.body;
 
-    //   switch case for different events
+    // Switch case for different events
     switch (type) {
       case "user.created": {
         const userData = {
@@ -40,7 +38,7 @@ const clerkWebHook = async (req, res) => {
           name: data.first_name + " " + data.last_name,
           image: data.image_url,
         };
-        await User.findByIdAndUpdate(userData.id, userData);
+        await User.findByIdAndUpdate(data.id, userData); // Corrected to use `data.id`
         res.json({});
         break;
       }
@@ -50,11 +48,12 @@ const clerkWebHook = async (req, res) => {
         break;
       }
       default:
+        res.status(400).json({ success: false, message: "Unknown event type" });
         break;
     }
   } catch (error) {
     console.log(error.message);
-    res.json({ success: false, message: "Web hooks error" });
+    res.status(500).json({ success: false, message: "Webhook error" });
   }
 };
 

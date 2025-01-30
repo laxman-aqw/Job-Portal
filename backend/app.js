0712f28aeca928160e0ac0868758880c2b53ed98
Laxman Rumba <rumbavai767@gmail.com>
2025-01-30 23:19:26 +0545

    removed sentry

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,9 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const connectDB = require("./config/db");
-const Sentry = require("@sentry/node");
+
 const { clerkWebHook } = require("./controllers/webhooks");
-// const companyRoutes = require("./routes/company-routes");
-require("./config/instrument");
+
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
@@ -15,15 +14,7 @@ app.get("/", (req, res) => {
   res.send("Hello World from laxman");
 });
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-
 app.post("/webhooks", clerkWebHook);
-
-// app.use("/api/company", companyRoutes);
-
-Sentry.setupExpressErrorHandler(app);
 
 connectDB().then(() => {
   app.listen(port, () => {
